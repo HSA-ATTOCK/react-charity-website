37ef7a2bb26a99f71335235fea5ed137a394597d
HSA-ATTOCK <haidersajjadazmat@gmail.com>
2025-06-30 01:48:47 +0500

    making changes for vercel

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve static files from the React app
+// Add CSP headers
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self'"
+  );
+  next();
+});
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "build")));
 
-// Handle React routing, return all requests to React app
+// Handle React routing
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
